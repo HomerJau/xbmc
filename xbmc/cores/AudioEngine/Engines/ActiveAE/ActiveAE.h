d81d2f08b372a87e7e359055afcdd6a841964cbf
HomerJau <Garry.Hobman@reva.net.au>
2026-01-12 12:56:33 +1100

    ActiveAE: Fix the Garry errors
    ActiveAE: missing initial 200ms of audio at playback start
    
    This was patched from Kodi 22 Master:
    ActiveAE: Fix missing initial ~200 ms of audio at playback start #27652
    This patch hard codes lowlatency = true and does on implement it Settings (it does in kodi 22)
    
    ActiveAE: Would not compile as ther were patched for LFE mixing
    
    Previous Check-in failed to compile as the as a patch to ActiveAE
    Added the patched code (that was in the Kodi official patch) and removed the ActiveAE patch code.from the official patch
    
    [Settings][Audio] Add new GUI setting for "Low latency mode"
    
    Added the full master code for the new Low Latency mode - users can now disable Audio Settings

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.h
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.h
@@ -63,6 +63,8 @@ struct AudioSettings
   double atempoThreshold;
   bool streamNoise;
   int silenceTimeoutMinutes;
+  float mixSubLevel; 
+  bool lowLatencyMode; 
 };
 
 class CActiveAEControlProtocol : public Protocol
@@ -407,5 +409,7 @@ protected:
   float m_aeVolume;
   bool m_aeMuted;
   bool m_aeGUISoundForce;
+
+  float m_targetBufferLevel{0.0f};
 };
 };
