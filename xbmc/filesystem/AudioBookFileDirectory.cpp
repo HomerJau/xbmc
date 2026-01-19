0501391d24a016297531bc70b9127e099b66b46b
HomerJau <Garry.Hobman@reva.net.au>
2026-01-19 17:27:14 +1100

    Matroska Tagging (another duration fix)

--- xbmc/filesystem/AudioBookFileDirectory.cpp
+++ xbmc/filesystem/AudioBookFileDirectory.cpp
@@ -455,8 +455,8 @@ bool CAudioBookFileDirectory::GetDirectory(const CURL& url,
     /* Attempt to fix no duration in singla chaptered MKA */
     if (m_fctx->nb_chapters == 1)
     {
-      const int Duration = fctx->streams[0]->duration * av_q2d(fctx->streams[0]->time_base);
-      item->GetMusicInfoTag()->SetDuration(Duration)
+      const int Duration = m_fctx->streams[0]->duration * av_q2d(m_fctx->streams[0]->time_base);
+      item->GetMusicInfoTag()->SetDuration(Duration);
     }
   
     item->SetProperty("item_start", item->GetStartOffset());
