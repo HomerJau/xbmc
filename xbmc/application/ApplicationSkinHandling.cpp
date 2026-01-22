99c93684d0c5ed196dc2d931e90a08c6b2dbdb6e
HomerJau <Garry.Hobman@reva.net.au>
2026-01-23 10:01:35 +1100

    [skin] Check GUI in reloadskin
    fix_crash_reload_skin

--- xbmc/application/ApplicationSkinHandling.cpp
+++ xbmc/application/ApplicationSkinHandling.cpp
@@ -376,14 +376,18 @@ bool CApplicationSkinHandling::LoadCustomWindows()
 
 void CApplicationSkinHandling::ReloadSkin(bool confirm)
 {
-  if (!g_SkinInfo || m_bInitializing)
+  auto gui = CServiceBroker::GetGUI();
+  if (gui == nullptr)
+    return;
+
+  auto skin = gui->GetSkinInfo();
+  if (!skin || m_bInitializing)
     return; // Don't allow reload before skin is loaded by system
 
-  std::string oldSkin = g_SkinInfo->ID();
+  std::string oldSkin = skin->ID();
 
-  CGUIMessage msg(GUI_MSG_LOAD_SKIN, -1,
-                  CServiceBroker::GetGUI()->GetWindowManager().GetActiveWindow());
-  CServiceBroker::GetGUI()->GetWindowManager().SendMessage(msg);
+  CGUIMessage msg(GUI_MSG_LOAD_SKIN, -1, gui->GetWindowManager().GetActiveWindow());
+  gui->GetWindowManager().SendMessage(msg);
 
   const std::shared_ptr<CSettings> settings = CServiceBroker::GetSettingsComponent()->GetSettings();
   std::string newSkin = settings->GetString(CSettings::SETTING_LOOKANDFEEL_SKIN);
@@ -400,7 +404,7 @@ void CApplicationSkinHandling::ReloadSkin(bool confirm)
         settings->SetString(CSettings::SETTING_LOOKANDFEEL_SKIN, oldSkin);
       }
       else
-        CServiceBroker::GetGUI()->GetWindowManager().ActivateWindow(WINDOW_STARTUP_ANIM);
+        gui->GetWindowManager().ActivateWindow(WINDOW_STARTUP_ANIM);
     }
   }
   else
