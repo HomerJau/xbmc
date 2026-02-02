94510c870a4fb0029f21cea63424bb66dbf03117
HomerJau <Garry.Hobman@reva.net.au>
2026-02-03 08:16:51 +1100

    Revert "Merge pull request #27741 from pannal/guilib-7"
    This reverts commit c1a49279b260064a6a5be80288f56bf83f3f7ecf.

--- xbmc/guilib/GUIBorderedImage.h
+++ xbmc/guilib/GUIBorderedImage.h
@@ -12,8 +12,6 @@
 #include "GUIImage.h"
 #include "TextureManager.h"
 
-#include <optional>
-
 class CGUIBorderedImage : public CGUIImage
 {
 public:
@@ -32,7 +30,6 @@ public:
 protected:
   std::unique_ptr<CGUITexture> m_borderImage;
   CRect m_borderSize;
-  std::optional<CRect> m_lastBorderRect;
 
 private:
   CGUIBorderedImage(const CGUIBorderedImage& right);
