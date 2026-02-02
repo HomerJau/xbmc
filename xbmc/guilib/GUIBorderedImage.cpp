f03812313e3ac9c85bbfdb8759fec6fb56ccfa27
HomerJau <Garry.Hobman@reva.net.au>
2026-02-03 08:17:52 +1100

    Revert "Reapply "Merge pull request #27741 from pannal/guilib-7""
    This reverts commit a25240ee50d102846dcc02acd47ba77e06401f81.

--- xbmc/guilib/GUIBorderedImage.cpp
+++ xbmc/guilib/GUIBorderedImage.cpp
@@ -39,21 +39,15 @@ CGUIBorderedImage::CGUIBorderedImage(const CGUIBorderedImage& right)
 void CGUIBorderedImage::Process(unsigned int currentTime, CDirtyRegionList &dirtyregions)
 {
   CGUIImage::Process(currentTime, dirtyregions);
-  if (!m_borderImage->GetFileName().empty() && m_textureCurrent->ReadyToRender())
+  if (!m_borderImage->GetFileName().empty() && m_texture->ReadyToRender())
   {
-    CRect rect = CRect(m_textureCurrent->GetXPosition(), m_textureCurrent->GetYPosition(),
-                       m_textureCurrent->GetXPosition() + m_textureCurrent->GetWidth(),
-                       m_textureCurrent->GetYPosition() + m_textureCurrent->GetHeight());
-    rect.Intersect(m_textureCurrent->GetRenderRect());
-
-    if (m_lastBorderRect != rect)
-    {
-      m_borderImage->SetPosition(rect.x1 - m_borderSize.x1, rect.y1 - m_borderSize.y1);
-      m_borderImage->SetWidth(rect.Width() + m_borderSize.x1 + m_borderSize.x2);
-      m_borderImage->SetHeight(rect.Height() + m_borderSize.y1 + m_borderSize.y2);
-      m_lastBorderRect = rect;
-    }
-
+    CRect rect = CRect(m_texture->GetXPosition(), m_texture->GetYPosition(),
+                       m_texture->GetXPosition() + m_texture->GetWidth(),
+                       m_texture->GetYPosition() + m_texture->GetHeight());
+    rect.Intersect(m_texture->GetRenderRect());
+    m_borderImage->SetPosition(rect.x1 - m_borderSize.x1, rect.y1 - m_borderSize.y1);
+    m_borderImage->SetWidth(rect.Width() + m_borderSize.x1 + m_borderSize.x2);
+    m_borderImage->SetHeight(rect.Height() + m_borderSize.y1 + m_borderSize.y2);
     m_borderImage->SetDiffuseColor(m_diffuseColor);
     if (m_borderImage->Process(currentTime))
       MarkDirtyRegion();
@@ -62,17 +56,9 @@ void CGUIBorderedImage::Process(unsigned int currentTime, CDirtyRegionList &dirt
 
 void CGUIBorderedImage::Render()
 {
-  bool renderFrontToBack = CServiceBroker::GetWinSystem()->GetGfxContext().GetRenderOrder() ==
-                           RENDER_ORDER_FRONT_TO_BACK;
-
-  if (renderFrontToBack)
-    CGUIImage::Render();
-
-  if (!m_borderImage->GetFileName().empty() && m_textureCurrent->ReadyToRender())
-    m_borderImage->Render(-1);
-
-  if (!renderFrontToBack)
-    CGUIImage::Render();
+  if (!m_borderImage->GetFileName().empty() && m_texture->ReadyToRender())
+    m_borderImage->Render();
+  CGUIImage::Render();
 }
 
 CRect CGUIBorderedImage::CalcRenderRegion() const
@@ -85,14 +71,12 @@ void CGUIBorderedImage::AllocResources()
 {
   m_borderImage->AllocResources();
   CGUIImage::AllocResources();
-  m_lastBorderRect.reset();
 }
 
 void CGUIBorderedImage::FreeResources(bool immediately)
 {
   m_borderImage->FreeResources(immediately);
   CGUIImage::FreeResources(immediately);
-  m_lastBorderRect.reset();
 }
 
 void CGUIBorderedImage::DynamicResourceAlloc(bool bOnOff)
