f44601e519c14722a33ebb3569e02b99a1816938
HomerJau <Garry.Hobman@reva.net.au>
2026-04-05 18:45:52 +1000

    Merge pull request xbmc#27976 from CrystalP/fix-slow-refresh
    Merge pull request xbmc#27976 from CrystalP/fix-slow-refresh
    Fix crash and art issues caused by DialogVideoInfo item changes

--- xbmc/video/windows/GUIWindowVideoBase.cpp
+++ xbmc/video/windows/GUIWindowVideoBase.cpp
@@ -507,7 +507,7 @@ bool CGUIWindowVideoBase::ShowInfoAndRefresh(const CFileItemPtr& item, const Scr
   if (ret && IsActive())
   {
     const int itemNumber{m_viewControl.GetSelectedItem()};
-    Refresh();
+    Refresh(true);
     m_viewControl.SetSelectedItem(itemNumber);
   }
 
