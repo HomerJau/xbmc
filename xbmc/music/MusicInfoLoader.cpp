84114066b77b0a163618fb08ddda734e12e07cf9
HomerJau <Garry.Hobman@reva.net.au>
2026-04-06 12:31:06 +1000

    Open [Music] Fix issues relating to CUE sheets.
    From Master April 2026

--- xbmc/music/MusicInfoLoader.cpp
+++ xbmc/music/MusicInfoLoader.cpp
@@ -26,6 +26,8 @@
 #include "utils/URIUtils.h"
 #include "utils/log.h"
 
+#include <ranges>
+
 using namespace XFILE;
 using namespace MUSIC_INFO;
 
@@ -160,21 +162,22 @@ bool CMusicInfoLoader::LoadItemCached(CFileItem* pItem)
 
 bool CMusicInfoLoader::LoadItemLookup(CFileItem* pItem)
 {
-  if (m_pProgressCallback && !pItem->m_bIsFolder)
+  if (m_pProgressCallback && !pItem->IsFolder())
     m_pProgressCallback->SetProgressAdvance();
 
-  if ((pItem->m_bIsFolder && !pItem->IsAudio()) || //
-      pItem->IsPlayList() || pItem->IsSmartPlayList() || //
+  if ((pItem->IsFolder() && !MUSIC::IsAudio(*pItem)) || //
+      PLAYLIST::IsPlayList(*pItem) || PLAYLIST::IsSmartPlayList(*pItem) || //
       StringUtils::StartsWithNoCase(pItem->GetPath(), "newplaylist://") || //
       StringUtils::StartsWithNoCase(pItem->GetPath(), "newsmartplaylist://") || //
-      pItem->IsNFO() || (pItem->IsInternetStream() && !pItem->IsMusicDb()))
+      pItem->IsNFO() || (NETWORK::IsInternetStream(*pItem) && !MUSIC::IsMusicDb(*pItem)))
     return false;
 
-  if ((!pItem->HasMusicInfoTag() || !pItem->GetMusicInfoTag()->Loaded()) && pItem->IsAudio())
+  if ((!pItem->HasMusicInfoTag() || !pItem->GetMusicInfoTag()->Loaded()) && MUSIC::IsAudio(*pItem))
   {
     // first check the cached item
     CFileItemPtr mapItem = (*m_mapFileItems)[pItem->GetPath()];
-    if (mapItem && mapItem->m_dateTime==pItem->m_dateTime && mapItem->HasMusicInfoTag() && mapItem->GetMusicInfoTag()->Loaded())
+    if (mapItem && mapItem->HasMusicInfoTag() && mapItem->GetMusicInfoTag()->Loaded() &&
+        mapItem->GetDateTime() == pItem->GetDateTime())
     { // Query map if we previously cached the file on HD
       *pItem->GetMusicInfoTag() = *mapItem->GetMusicInfoTag();
       if (mapItem->HasArt("thumb"))
@@ -184,7 +187,7 @@ bool CMusicInfoLoader::LoadItemLookup(CFileItem* pItem)
     {
       std::string strPath = URIUtils::GetDirectory(pItem->GetPath());
       URIUtils::AddSlashAtEnd(strPath);
-      if (strPath!=m_strPrevPath)
+      if (strPath != m_strPrevPath)
       {
         // The item is from another directory as the last one,
         // query the database for the new directory...
@@ -192,16 +195,8 @@ bool CMusicInfoLoader::LoadItemLookup(CFileItem* pItem)
         m_databaseHits++;
       }
 
-      /*
-      This only loads the item with the song from the database when it maps to a single song,
-      it can not load song data for items with cuesheets that expand to multiple songs.
-      For songs from embedded or separate cuesheets strFileName is not unique, so the song map for
-      the path will have the list of songs from that file. But items with cuesheets are expanded
-      (replacing each item with items for every track) elsewhere. When the item we are looking up
-      has a cuesheet document or is a music file with a cuesheet embedded in the tags, and it maps
-      to more than one song then we can not fill the tag data and thumb from the database.
-      */
-      MAPSONGS::iterator it = m_songsMap.find(pItem->GetPath()); // Find file in song map
+      const auto it = m_songsMap.find(pItem->GetPath()); // Find file in song map
+            
       if (it != m_songsMap.end() && it->second.size() == 1)
       {
         // Have we loaded this item from database before,
@@ -210,23 +205,39 @@ bool CMusicInfoLoader::LoadItemLookup(CFileItem* pItem)
         if (!it->second[0].strThumb.empty())
           pItem->SetArt("thumb", it->second[0].strThumb);
       }
-      else if (pItem->IsMusicDb())
-      { // a music db item that doesn't have tag loaded - grab details from the database
-        XFILE::MUSICDATABASEDIRECTORY::CQueryParams param;
-        XFILE::MUSICDATABASEDIRECTORY::CDirectoryNode::GetDatabaseInfo(pItem->GetPath(),param);
-        CSong song;
-        if (m_musicDatabase.GetSong(param.GetSongId(), song))
+      else if (it != m_songsMap.end() && it->second.size() > 1 &&
+               pItem->GetProperty("cueloadinformation").asBoolean(false))
+      {
+        // Find matching song
+        const auto& songs{it->second};
+        const auto it2{std::ranges::find_if(
+            songs,
+            [&pItem](const CSong& song)
+            {
+              return song.iStartOffset == static_cast<int>(pItem->GetStartOffset()) &&
+                     song.iEndOffset == static_cast<int>(pItem->GetEndOffset());
+            })};
+        if (it2 != songs.end())
         {
-          pItem->GetMusicInfoTag()->SetSong(song);
-          if (!song.strThumb.empty())
-            pItem->SetArt("thumb", song.strThumb);
+          // Populate the music info tag from the matched song
+          pItem->GetMusicInfoTag()->SetSong(*it2);
+          if (!it2->strThumb.empty())
+            pItem->SetArt("thumb", it2->strThumb);
+
+          // Build the musicdb:// path so the item references the database entry
+          pItem->SetDynPath(pItem->GetPath());
+          pItem->SetPath(StringUtils::Format("musicdb://songs/{}{}", it2->idSong,
+                                             URIUtils::GetExtension(it2->strFileName)));
         }
       }
-      else if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_MUSICFILES_USETAGS) || pItem->IsCDDA())
+      else if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(
+                   CSettings::SETTING_MUSICFILES_USETAGS) ||
+               MUSIC::IsCDDA(*pItem))
       { // Nothing found, load tag from file,
         // always try to load cddb info
         // get correct tag parser
-        std::unique_ptr<IMusicInfoTagLoader> pLoader (CMusicInfoTagLoaderFactory::CreateLoader(*pItem));
+        std::unique_ptr<IMusicInfoTagLoader> pLoader(
+            CMusicInfoTagLoaderFactory::CreateLoader(*pItem));
         if (nullptr != pLoader)
           // get tag
           pLoader->Load(pItem->GetPath(), *pItem->GetMusicInfoTag());
