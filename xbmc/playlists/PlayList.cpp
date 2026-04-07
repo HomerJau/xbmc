fccee99654181b01dc3f00ab0a14434c24e263f5
HomerJau <Garry.Hobman@reva.net.au>
2026-04-08 04:37:51 +1000

    Playlist fix (cue sheets)

--- xbmc/playlists/PlayList.cpp
+++ xbmc/playlists/PlayList.cpp
@@ -481,6 +481,7 @@ bool CPlayList::Expand(int position)
     if (!(*playlist)[i]->HasProperty("item_start"))
       (*playlist)[i]->SetStartOffset(item->GetStartOffset());
     if (!(*playlist)[i]->HasProperty("BasePath"))
+      (*playlist)[i]->SetProperty("BasePath", playlist->m_strBasePath);
   }
 
   if (playlist->size() <= 0)
